af07fc8519848c13f52e80891da27d91ff05afd1
sakshich0113 <115971727+sakshich0113@users.noreply.github.com>
2022-10-17 11:31:02 +0530

    Create powoftwo.cpp

---
+++ C++/powoftwo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int num;
+
+   cout << "Enter the number you want to test: ";
+   cin >> num;
+
+   //condition to check whether number is power of two or not
+   //performing bitwise operation
+   if((num != 0) && ((num &(num - 1)) == 0))
+      cout << num << " is a power of 2." << endl;
+
+   else
+      cout << num << " is not a power of 2." << endl;
+
+   return 0;
+}
